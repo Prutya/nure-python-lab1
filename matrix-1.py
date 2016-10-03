f0b7d8f5e16e9c980b57dc749d7fabe9fc43e968
Anton Prudkohliad <anton.prudkohliad@gmail.com>
2016-10-03 15:07:48 +0300

    Vivel matricu na ekran

---
+++ matrix-1.py
@@ -0,0 +1,8 @@
+matrica = [[0, 0, 0, 1],
+           [0, 0, 0, 1],
+           [0, 0, 0, 1]]
+
+print(matrica) # Tak?
+
+for stroka in matrica:
+    print(", ".join(str(chislo) for chislo in stroka)) # Ili tak
